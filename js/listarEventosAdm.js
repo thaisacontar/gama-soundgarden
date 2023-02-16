52d475d0a37c64e9eb24e42172f2917173ed7091
Thai Esseà <thaisacontar@gmail.com>
2023-02-16 12:27:05 -0400

    Adicionando pasta do projeto ao repositório GitHub

---
+++ js/listarEventosAdm.js
@@ -0,0 +1,21 @@
+// VARIÁVEIS 
+
+const admEventos = { // criar um objeto com os dados necessários vindos da API (separados por vírgula)
+  nomeEvento : document.getElementById('nome'),
+  atracoesEvento : document.getElementById('atracoes'),
+  descricaoEvento : document.getElementById('descricao'),
+  dataEvento : document.getElementById('data'),
+  lotacaoEvento : document.getElementById('lotacao'),
+  formularioCadastro : document.getElementById('formulario')
+}
+
+const dados = { // transformar o objeto em strings (para JSON) adicionando .value em cada
+  "nome": nomeEvento.value,
+  "atracoes": atracoesEvento.value,
+  "descricao": descricaoEvento.value,
+  "agenda": dataEvento.value,
+  "capacidade": lotacaoEvento.value,
+};
+
+const apiURL = 'https://soundgarden-api.vercel.app/events';
+
